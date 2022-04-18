8ff931a9b5112ad0bf2fd0ef2376f9ff95d77c14
abril ramirez <abril128lb@gmail.com>
2022-04-17 18:54:17 -0600

    final

---
+++ build/precache-manifest.19f25b033978de400fb2b3435901e582.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9373a1f39d2668a090a1",
+    "url": "/static/css/main.f77d3eba.chunk.css"
+  },
+  {
+    "revision": "9373a1f39d2668a090a1",
+    "url": "/static/js/main.9373a1f3.chunk.js"
+  },
+  {
+    "revision": "f7e1b4706d1f3d796bdf",
+    "url": "/static/js/1.f7e1b470.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d7f4158bd84c6c7d83927e62bf535b3",
+    "url": "/index.html"
+  }
+];
